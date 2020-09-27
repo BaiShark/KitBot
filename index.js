719a42acad2ae91405fa817bcee8711978517f55
Kitam <whitehacker638@gmail.com>
2020-09-27 14:37:55 +0300

    Добавлены файлы для хостинга Heroku

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
 
 const client = new Discord.Client();
 
 require('./handlers/eventHandler')(client);
 require('./handlers/commandHandler')(client);
 
-client.login(config.token);
+client.login(process.env.token);
