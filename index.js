a6b23dee9d8893bafb08df26530a9c19bb724dde
Kitam <whitehacker638@gmail.com>
2020-10-26 17:40:39 +0200

    Обновлён index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const client = new Discord.Client();
 require('./handlers/eventHandler')(client);
 require('./handlers/commandHandler')(client);
 
-client.login("NzUzMjY3Mzc3OTE1MDM1ODE5.X1js6Q.QHNMAeUZ9JXIS8HZpYOE0LmIhA8");
+client.login(process.env.TOKEN);
