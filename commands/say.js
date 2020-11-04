b9aa23144c07fd75489db574aa79bb5d92efbb94
Kitam <whitehacker638@gmail.com>
2020-11-04 10:13:43 +0200

    Updated say.js

--- commands/say.js
+++ commands/say.js
@@ -8,10 +8,8 @@ module.exports = {
     description: 'Вывести сообщение от имени бота.',
     async execute(message, args) {
         if (!message.member.roles.cache.get(config.adminRole)) return;
-        if (message.deletable) message.delete();
-
         if (!args[0]) return message.channel.send(error.embed('Вы не указали отправляемое сообщение.'));
-
+        if (message.deletable) message.delete();
         return message.channel.send(args.join(' '));
     }
 };
