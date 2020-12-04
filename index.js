afaf1f7e1caa10fb36abc5b42b67cc96b75e0152
Kitaminka <whitehacker638@gmail.com>
2020-12-04 17:49:11 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ client.player = player;
 require('./handlers/eventHandler')(client);
 require('./handlers/commandHandler')(client);
 
-client.login(process.env.token);
+client.login(process.env.TOKEN);
