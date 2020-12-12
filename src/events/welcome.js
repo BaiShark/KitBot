1a76debee46b089033d56484451e4ea0ecdf2517
BaiShark <75904243+BaiShark@users.noreply.github.com>
2020-12-12 20:43:51 +0400

    Update welcome.js

--- src/events/welcome.js
+++ src/events/welcome.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
-const config = require('../../config.json');
+const config = require('../config.json');
 
 module.exports = async (member) => {
     if (member.user.bot) return;
 
-    member.roles.add(config.verificationRole);
 
     const canvas = Canvas.createCanvas(700, 250);
     const ctx = canvas.getContext('2d');
-    Canvas.registerFont('../../public/fonts/impact.ttf', { family: 'Impact' });
 
     const background = await Canvas.loadImage('./img/background.jpg');
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
@@ -29,37 +27,11 @@ module.exports = async (member) => {
 
     const embed = new Discord.MessageEmbed()
         .setTitle(':wave:Добро пожаловать!')
-        .setDescription(`Добро пожаловать на сервер, ${member}. Надеюсь, тебе тут понравится.`)
+        .setDescription(`${member} Добро пожаловать на сервер, **MagmaPixel**. Надеюсь, тебе тут понравится.`)
         .attachFiles([new Discord.MessageAttachment(canvas.toBuffer(), 'welcome_image.png')])
-        .setFooter('Чтобы пройти верификацию, нажмите реакцию ниже', 'https://i.imgur.com/DyWlZOU.png')
         .setImage('attachment://welcome_image.png')
         .setTimestamp()
         .setColor(config.embedColor);
 
-    const welcomeMessage = await member.guild.channels.cache.get(config.welcomeChannel).send(embed);
-    await welcomeMessage.react('✅');
-
-    const filter = (reaction, user) => {
-        return '✅'.includes(reaction.emoji.name) && user.id === member.user.id;
-    };
-
-    welcomeMessage.awaitReactions(filter, { max: 1, time: 300000, errors: ['time'] })
-        .then( () => {
-            const editedMessage = new Discord.MessageEmbed()
-                .setTitle(':wave:Добро пожаловать!')
-                .setDescription(`Добро пожаловать на сервер, ${member}. Надеюсь, тебе тут понравится.`)
-                .attachFiles([new Discord.MessageAttachment(canvas.toBuffer(), 'welcome_image.png')])
-                .setFooter('Верификация успешно пройдена', 'https://i.imgur.com/DyWlZOU.png')
-                .setImage('attachment://welcome_image.png')
-                .setTimestamp()
-                .setColor(config.embedColor);
-
-            member.roles.remove(config.verificationRole);
-            welcomeMessage.edit(editedMessage);
-            welcomeMessage.reactions.removeAll();
-        })
-        .catch( () => {
-            welcomeMessage.delete();
-            if (member) member.kick('Верификация не была пройдена в течении 5 минут');
-        });
-}
+return member.guild.channels.cache.get(config.welcomeChannel).send(embed);
+    }
