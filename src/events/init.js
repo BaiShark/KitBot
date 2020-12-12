95edeec6ce2cb0d623cf2fb065e4cdf879832306
BaiShark <75904243+BaiShark@users.noreply.github.com>
2020-12-12 20:45:39 +0400

    Update init.js

--- src/events/init.js
+++ src/events/init.js
@@ -1,6 +1,6 @@
 const config = require('../../config.json');
 
 module.exports = async (client) => {
-    await client.user.setActivity(`${config.prefix}help`);
+    await client.user.setActivity(`MagmaPixel`);
     console.log(`init.js: ${client.user.username} started!`);
 }
